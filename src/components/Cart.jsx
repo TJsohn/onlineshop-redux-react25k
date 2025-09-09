efeba4bd73d96a22cf047de2fc6bf40c9a42812b
TJsohn <sohntaejeong@gmail.com>
2025-09-09 22:32:01 +0300

    add add cart and remove from cart functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,10 @@
-import { useAppSelector } from '../hooks/hooks';
+import { useAppDispatch, useAppSelector } from '../hooks/hooks';
+import { Typography } from '@mui/material';
+import { removeFromCart } from '../store/cartSlice';
 
 function Cart() {
   const cartItems = useAppSelector(state => state.cart.cart);
+  const dispatch = useAppDispatch();
 
   return (
     <div>
@@ -12,7 +15,10 @@ function Cart() {
         <ul>
           {cartItems.map(item => (
             <li key={item.id}>
-              {item.name} (x{item.quantity})
+              {item.title} (x{item.quantity})
+              <Typography variant="body2">
+                <button onClick={() => dispatch(removeFromCart(item))}>Remove</button>
+              </Typography>
             </li>
           ))}
         </ul>
